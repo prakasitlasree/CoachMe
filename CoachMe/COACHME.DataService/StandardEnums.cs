8a1ab138e35022383865da08661faebb8daecadb
Natchaphon <Natchaphon@DESKTOP-F419P0B>
2018-09-24 00:54:21 +0700

    stardardEnum

---
+++ CoachMe/COACHME.DataService/StandardEnums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COACHME.DATASERVICE
+{
+   public static class StandardEnums
+    {
+       public enum ConfigurationSettingName
+        {
+            MAIL_SENDER,
+            MAIL_SUBJECT,
+            MAIL_BODY, 
+            MAIL_FOOTER,
+            SMTP_ACCOUNT,
+            SMTP_PASSWORD,
+                FORGET_PASSWORD_URL
+        }
+    }
+}
